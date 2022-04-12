ed39387fc4fd72bc1357a985bcbf71b89018d91b
刘富国 <595726017@qq.com>
2022-04-12 13:47:42 +0800

    style: 格式化,处理告警

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-
-var words = require('shellwords')
+const words = require('shellwords');
 
 // TODO -F, --form
 // TODO --data-binary
@@ -10,87 +9,87 @@ var words = require('shellwords')
  * Attempt to parse the given curl string.
  */
 
-module.exports = exports.default = function(s) {
-  if (0 != s.indexOf('curl ')) return
-  var args = rewrite(words.split(s))
-  var out = { method: 'GET', header: {} }
-  var state = ''
+module.exports = exports.default = function (s) {
+    if (0 !== s.indexOf('curl ')) return
+    const args = rewrite(words.split(s));
+    const out = {method: 'GET', url: '', header: {}, body: ''};
+    let state = '';
 
-  args.forEach(function(arg){
-    switch (true) {
-      case isURL(arg):
-        out.url = arg
-        break;
+    args.forEach(function (arg) {
+        switch (true) {
+            case isURL(arg):
+                out.url = arg
+                break;
 
-      case arg == '-A' || arg == '--user-agent':
-        state = 'user-agent'
-        break;
+            case arg === '-A' || arg === '--user-agent':
+                state = 'user-agent'
+                break;
 
-      case arg == '-H' || arg == '--header':
-        state = 'header'
-        break;
+            case arg === '-H' || arg === '--header':
+                state = 'header'
+                break;
 
-      case arg == '-d' || arg == '--data' || arg == '--data-ascii' || arg == '--data-raw':
-        state = 'data'
-        break;
+            case arg === '-d' || arg === '--data' || arg === '--data-ascii' || arg === '--data-raw':
+                state = 'data'
+                break;
 
-      case arg == '-u' || arg == '--user':
-        state = 'user'
-        break;
+            case arg === '-u' || arg === '--user':
+                state = 'user'
+                break;
 
-      case arg == '-I' || arg == '--head':
-        out.method = 'HEAD'
-        break;
+            case arg === '-I' || arg === '--head':
+                out.method = 'HEAD'
+                break;
 
-      case arg == '-X' || arg == '--request':
-        state = 'method'
-        break;
+            case arg === '-X' || arg === '--request':
+                state = 'method'
+                break;
 
-      case arg == '-b' || arg =='--cookie':
-        state = 'cookie'
-        break;
+            case arg === '-b' || arg === '--cookie':
+                state = 'cookie'
+                break;
 
-      case arg == '--compressed':
-        out.header['Accept-Encoding'] = out.header['Accept-Encoding'] || 'deflate, gzip'
-        break;
+            case arg === '--compressed':
+                out.header['Accept-Encoding'] = out.header['Accept-Encoding'] || 'deflate, gzip'
+                break;
 
-      case !!arg:
-        switch (state) {
-          case 'header':
-            var field = parseField(arg)
-            out.header[field[0]] = field[1]
-            state = ''
-            break;
-          case 'user-agent':
-            out.header['User-Agent'] = arg
-            state = ''
-            break;
-          case 'data':
-            if (out.method == 'GET' || out.method == 'HEAD') out.method = 'POST'
-            out.header['Content-Type'] = out.header['Content-Type'] || 'application/x-www-form-urlencoded'
-            out.body = out.body
-              ? out.body + '&' + arg
-              : arg
-            state = ''
-            break;
-          case 'user':
-            out.header['Authorization'] = 'Basic ' + btoa(arg)
-            state = ''
-            break;
-          case 'method':
-            out.method = arg
-            state = ''
-            break;
-          case 'cookie':
-            out.header['Set-Cookie'] = arg
-            state = ''
-            break;
+            case !!arg:
+                switch (state) {
+                    case 'header':
+                        const field = parseField(arg);
+                        out.header[field[0]] = field[1]
+                        state = ''
+                        break;
+                    case 'user-agent':
+                        out.header['User-Agent'] = arg
+                        state = ''
+                        break;
+                    case 'data':
+                        if (out.method === 'GET' || out.method === 'HEAD') out.method = 'POST'
+                        out.header['Content-Type'] = out.header['Content-Type'] || 'application/x-www-form-urlencoded'
+                        out.body = out.body
+                            ? out.body + '&' + arg
+                            : arg
+                        state = ''
+                        break;
+                    case 'user':
+                        out.header['Authorization'] = 'Basic ' + btoa(arg)
+                        state = ''
+                        break;
+                    case 'method':
+                        out.method = arg
+                        state = ''
+                        break;
+                    case 'cookie':
+                        out.header['Set-Cookie'] = arg
+                        state = ''
+                        break;
+                }
+                break;
         }
-        break;
-    }
-  })
+    })
 
-  return out
+    return out
 }
 
 /**
@@ -98,16 +97,16 @@ module.exports = exports.default = function(s) {
  */
 
 function rewrite(args) {
-  return args.reduce(function(args, a){
-    if (0 == a.indexOf('-X')) {
-      args.push('-X')
-      args.push(a.slice(2))
-    } else {
-      args.push(a)
-    }
+    return args.reduce(function (args, a) {
+        if (0 === a.indexOf('-X')) {
+            args.push('-X')
+            args.push(a.slice(2))
+        } else {
+            args.push(a)
+        }
 
-    return args
-  }, [])
+        return args
+    }, [])
 }
 
 /**
@@ -115,7 +114,7 @@ function rewrite(args) {
  */
 
 function parseField(s) {
-  return s.split(/: (.+)/)
+    return s.split(/: (.+)/)
 }
 
 /**
@@ -123,6 +122,6 @@ function parseField(s) {
  */
 
 function isURL(s) {
-  // TODO: others at some point
-  return /^https?:\/\//.test(s)
+    // TODO: others at some point
+    return /^https?:\/\//.test(s)
 }
